99dfc8213cf9289a18f2548039c98d412b75dbcf
wanda belingga putra <45660041+WandaBelinggaPutra@users.noreply.github.com>
2019-01-05 19:15:48 +0700

    Add files via upload

---
+++ bilangan dengan fungsi.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+
+cout<<"PROGRAM MEMERIKSA BILANGAN TERBESAR DARI 2 BUAH BILANGAN"<<endl;
+
+cout<<"Masukan Bilangan Pertama = ";
+cin>>a;
+cout<<"Masukan Bilangan Kedua   = ";
+cin>>b;
+if(a<b){
+  cout<<"B Terbesar\n";}
+  else
+  if(a>b){
+  cout<<"A Terbesar\n";}
+  else
+    cout<<"Eror";
+  return 0;
+
+
+
+
+
+
+
+
+
+}
